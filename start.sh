d21017b43c8784f9afc3865fa4c890631cc61145
ROHIT12-bit <reddyrohit823@gmail.com>
2026-01-12 19:09:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-echo "Starting Playit..."
+# Download PaperMC if not exists
+if [ ! -f server.jar ]; then
+  echo "Downloading PaperMC..."
+  curl -L https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/493/downloads/paper-1.20.4-493.jar -o server.jar
+fi
+
+# Accept EULA
+echo "eula=true" > eula.txt
+
+# Download Playit
 curl -L https://playit.gg/downloads/playit-linux-amd64 -o playit
 chmod +x playit
-./playit &
 
-echo "Starting Minecraft..."
+# Start Playit tunnel
+./playit --secret "$PLAYIT_AGENT_KEY" &
+
+# Start Minecraft
 java -Xms512M -Xmx1G -jar server.jar nogui
